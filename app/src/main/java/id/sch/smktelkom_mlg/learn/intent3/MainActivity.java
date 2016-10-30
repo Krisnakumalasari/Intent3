55a8c0609f80d4ba95b440369c53f2e6a9055907
NanaShin <krisnakumalasari29@gmail.com>
2016-10-30 15:04:06 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import static android.R.attr.phoneNumber;
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
                         dialPhoneNumber("0341712500");
                     }
                 });
+
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
+    }
+
+    private void composeSmsMessage(String s) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void dialPhoneNumber(String s) {
